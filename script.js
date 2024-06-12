980a54b79fdce93682bf1fed9f264a8b2b56a41c
Wanj0k <111271074+Wanj0k@users.noreply.github.com>
2024-06-12 11:09:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,66 @@
-alert("hi");
-var schet = 0;
-jQuery('button').on('click', function(){
-    schet+=1;
-    jQuery('h2').html(schet);
+
+var energy = localStorage.getItem('energy_saved') || 300;
+var i = localStorage.getItem('i_saved') || 1;
+var up = localStorage.getItem('up_saved') || 100;;
+let a = localStorage.getItem('a_saved') || 0;
+var elb = localStorage.getItem('elb_saved') || 1;
+var ba = localStorage.getItem('ba_saved') || energy/ 3;
+
+
+
+
+
+
+
+
+
+jQuery('h2').html( Number (a));
+$("#cen").html(`цена улучшения: ${up}`);
+jQuery('.poi').on('click', function(){
+    ba = energy / 3;
+    a = Number(a)
+    i = Number(i)
+    a +=i;
+    
+    energy= Number(energy)
+
+    energy -=i;
+    localStorage.setItem('energy_saved', Number (energy));
+    localStorage.setItem('a_saved', Number (a));
+ 
+
+    jQuery('h2').html( Number (a));
 });
+
+
+$("#up").mousedown(function() {
+    if (a >= up){
+        a -= up;
+        localStorage.setItem('a_saved', Number (a));
+
+        up *=2
+        localStorage.setItem('up_saved', Number (up));
+        i+=1;
+        localStorage.setItem('i_saved', Number (i));
+
+    }
+
+        $("#cen").html(`цена улучшения: ${up}`);
+        jQuery('h2').html(a);
+    
+
+});
+//localStorage.clear();
+while(true){
+     
+    energy++;
+    ba = energy / 3;
+    $("#greenBar").width(`${ba}%`);
+    $("h3").html(energy); 
+
+
+
+    setTimeout(start, 1000); 
+}
+
+
